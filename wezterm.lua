a53011aacebbdd8c742e95dd531249e679a13d1e
Ryan Lee <Ryan.Lee@viasat.com>
2025-07-14 01:41:38 -0700

    bruh

--- wezterm.lua
+++ wezterm.lua
@@ -1,13 +1,15 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
+config = wezterm.config_builder()
+
+local is_linux = function()
+	return wezterm.target_triple:find("linux") ~= nil
+end
+
+local is_darwin = function()
+	return wezterm.target_triple:find("darwin") ~= nil
 end
 
 -- This is where you actually apply your config choices
@@ -22,26 +24,34 @@ end
 
 config.leader = {key = 'q', mods = 'CTRL' }
 config.disable_default_key_bindings = true
+
+cmd_key = is_darwin() and 'CMD' or 'CTRL'
+
 config.keys = {
-    { key = '+', mods = 'SHIFT|CTRL', action = wezterm.action.IncreaseFontSize },
-    { key = '-', mods = 'CTRL', action = wezterm.action.DecreaseFontSize },
-    { key = '0', mods = 'CTRL', action = wezterm.action.ResetFontSize },
-    { key = 'v', mods = 'SHIFT|CTRL', action = wezterm.action.PasteFrom 'Clipboard' },
-    { key = 'T', mods = 'SHIFT|CTRL', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
-    { key = 'W', mods = 'SHIFT|CTRL', action = wezterm.action.CloseCurrentTab {confirm = true} },
-    { key = "PageUp", mods = 'SHIFT|CTRL', action = wezterm.action.ActivateTabRelative(-1)},
-    { key = "PageDown", mods = 'SHIFT|CTRL', action = wezterm.action.ActivateTabRelative(1)},
+    { key = '+', mods = 'SHIFT|' .. cmd_key, action = wezterm.action.IncreaseFontSize },
+    { key = '-', mods = cmd_key, action = wezterm.action.DecreaseFontSize },
+    { key = '0', mods = cmd_key, action = wezterm.action.ResetFontSize },
+    { key = 'v', mods = 'SHIFT|' .. cmd_key, action = wezterm.action.PasteFrom 'Clipboard' },
+    { key = 'T', mods = 'SHIFT|' .. cmd_key, action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+    { key = 'W', mods = 'SHIFT|' .. cmd_key, action = wezterm.action.CloseCurrentTab {confirm = true} },
+    { key = "Tab", mods = is_darwin() and 'ALT' or 'CTRL', action = wezterm.action.ActivateTabRelative(-1)},
+    { key = "Tab", mods = is_darwin() and 'ALT' or 'CTRL', action = wezterm.action.ActivateTabRelative(1)},
 
     -- Panes
-    { key = ';', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-    { key = 'L', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
-    { key = 'H', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
-    { key = 'l', mods = 'LEADER', action = wezterm.action.AdjustPaneSize {'Right', 10 } },
-    { key = 'h', mods = 'LEADER', action = wezterm.action.AdjustPaneSize {'Left', 10} },
+    { key = '+', mods = 'ALT|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = '_', mods = 'ALT|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'h', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
+    { key = 'l', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
+    { key = 'k', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
+    { key = 'j', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down' },
+    { key = 'K', mods = 'ALT', action = wezterm.action.AdjustPaneSize {'Up', 5 } },
+    { key = 'J', mods = 'ALT', action = wezterm.action.AdjustPaneSize {'Down', 5} },
+    { key = 'H', mods = 'ALT', action = wezterm.action.AdjustPaneSize {'Left', 5 } },
+    { key = 'L', mods = 'ALT', action = wezterm.action.AdjustPaneSize {'Right', 5} },
 }
 
 config.font_size = 12.0
 
-config.term = 'wezterm'
+-- config.term = 'wezterm'
 -- and finally, return the configuration to wezterm
 return config
